2b112333d82061bcb4997c8ba6b3ea5d615d30fd
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2021-12-30 19:45:17 +0630

    logout

--- login.php
+++ login.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>login</title>
-</head>
-<body>
-     <?php
+<?php
           session_start();
           $id = $_POST['id'];
           $password = $_POST['password'];
 
-          if($id == "admim" && $password == "130070"){
+          if($id == "admin" && $password == "130070"){
                $_SESSION['auth'] = true;
                $_SESSION['id'] = "Admin";
           }
 
           header("location: index.php");
      ?>
-</body>
-</html>
